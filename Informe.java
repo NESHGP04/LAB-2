0412439aa9fac944b0eb2e4ff6aec5bfed12108a
NESHGP04 <89556682+NESHGP04@users.noreply.github.com>
2023-09-22 23:13:03 -0600

    2nd commit

--- Informe.java
+++ Informe.java
@@ -7,29 +7,37 @@ import java.util.ArrayList;
 import java.io.BufferedReader;  
 import java.io.FileReader;  
 import java.io.IOException;  
+import java.io.*;  
 
 public class Informe {
     public static void main(String[] args) {
+
+        //Asignación de variables
         Scanner sc = new Scanner(System.in);
         int idInput;
         ArrayList<Integer> cursoNoList = new ArrayList<Integer>();
         ArrayList<Integer> cursoSiList = new ArrayList<Integer>();
 
+        String[] salon;
+        boolean salir = false;
+
         String line = "";  
         String splitBy = ",";  
 
+        //Lee archivo CSV Salones y convierte a matriz
         System.out.println("\n<<<Archivo Salones>>>");
         try   {  
-            BufferedReader brSalon = new BufferedReader(new FileReader("Salones.csv"));  
+            BufferedReader brSalon = new BufferedReader(new FileReader("Salones.csv"));  //Lee salones.csv
             while ((line = brSalon.readLine()) != null){  
-                String[] salon = line.split(splitBy);    // use comma as separator  
-                System.out.println("\nId sede: " + salon[0] + "\nEdificio: " + salon[1] + "\nNivel: " + salon[2] + "\nId salón: " + salon[3] + "\nCapacidad: " + salon[4]);  
+                salon = line.split(splitBy);    // use comma as separator  
+                System.out.println("\nId sede: " + salon[0] + "\nEdificio: " + salon[1] + "\nNivel: " + salon[2] + "\nId salón: " + salon[3] + "\nCapacidad: " + salon[4]);  //Ordena datos separados por comas
                 }  
             System.out.println("\nSe ha cargado el archivo exitosamente!");        
         } catch (IOException e)   {  
                 e.printStackTrace();  
         } 
 
+        //Lee archivo CSV Cursos y convierte a matriz
         System.out.println("\n<<<Archivo Cursos>>>");
         try   {  
             BufferedReader brCurso = new BufferedReader(new FileReader("Cursos.csv"));  
@@ -41,15 +49,76 @@ public class Informe {
         } catch (IOException e)   {  
                 e.printStackTrace();  
         }  
+        
+        //Asignación de salones
+        System.out.println("\n==== ASIGNACIÓN DE SALONES ====");
+        
+        while(!salir){
 
+            //Menú
+            System.out.println("\n>>> MENÚ <<<");
+            System.out.println("1.Asignar curso \n2.Informe \n3.Salón");
+            int opc = sc.nextInt();
+            sc.nextLine();
 
+            switch(opc){
+                case 1:
+                    //Asignacion de cursos
+                    System.out.println("\n<<Asignación de cursos>>");
 
+                        //pide salón y busca en csv si salón disponible
+                    try{
+                        BufferedReader in = new BufferedReader(new InputStreamReader (System.in));
+                        BufferedReader reader = new BufferedReader(new FileReader("Salones.csv"));
+                        System.out.println("Ingrese el salón que desea asifnarse: ");
+                        String asignar = in.readLine();
+                        
+                        boolean found = false;
+                        String linea = null;
 
-
-
-
-
-
+                        while((linea=reader.readLine())!=null){
+                            String[] parts = linea.split(",");
+                            int totalParts = parts.length;
+                            for(int i = 0; i< totalParts; i++){
+                                if(asignar.compareTo(parts[i]) == 0){
+                                    System.out.println("Se ha encontrado su salón.");
+                                    found = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if(found == false){
+                            System.out.println("No se ha encontrado el salón.");
+                        }else{
+                            System.out.println("Se ha asignado correctamente!");
+                        }
+                    }catch(IOException e){
+                        e.printStackTrace();
+                        }
+                    break;
+                    
+                case 2:
+                //                        Imprimir informes
+                //            for(int i=0 ;i < cursoNoList.size() +cursoSiList.size()+1; ++i ){
+                //                    System.out.print(cursoNoList.get(i));
+                //                        System.out.print(",");
+                //                            System.out.print(cursoSiList.get(i-1));
+                //                                }
+                //            imprimirInforme(cursoNoList,cursolist);
+                //           
+                    break;
+                case 3:
+                    System.out.println("Ha salido del programa!");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Ingrese una opción válida");
+            }
+        }
+        
+        //        for (int i=0 ;i<=cursoNoList.size()-1;i++){
+            //            if (!cursoNoList[i].equals(""))
+            //                System.out.print(cursoNoList + " ");
         sc.close();
     }
 }
--- Salones.java
+++ Salones.java
@@ -3,6 +3,87 @@
  * CLASE
  */
 
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Salones {
+
+    private String disponible;
+    private int capacidad;
+    private boolean ocupado;
+    String[][] datosCSV = deCSVmatriz("Salones.csv");
+
+    private Scanner s;
     
+    public Salones(String disponible, int capacidad, boolean ocupado){
+        this.disponible = disponible;
+        this.capacidad = capacidad;
+        this.ocupado = ocupado;
+    }
+
+    //Lee archivo CSV por partes
+    public void setSalones(){
+        try{
+            File f = new File("Salones.csv");
+            s = new Scanner (f);
+            String line = s.nextLine();
+            String[] parts1 = line.split(",");
+                System.out.println(parts1[0]+" "+parts1[1]+" "+parts1[2]+" "+parts1[3]+" "+parts1[4]+" "+parts1[5]+" "+parts1[6]);
+                System.out.println("------------------------------");
+
+            while(s.hasNextLine()){
+                line = s.nextLine();
+                String[] parts = line.split(",");
+                String[][] matriz = new String[10][7];
+                System.out.println(parts[0]+" "+parts[1]+" "+parts[2]+" "+parts[3]+" "+parts[4]+" "+parts[5]+" "+parts[6]);
+                } 
+            }catch(FileNotFoundException e){
+        System.out.println("el archivo no existe");
+        }  
+    }
+
+    //Lee archivo CSV y convierte a matriz para ser utilizada después
+    public static String[][] deCSVmatriz(String rutaSCV){
+        try{
+            Scanner lector = new Scanner(new File(rutaSCV));
+            String[][] matriz = new String[30][7];
+            int fila = 0;
+            String linea = lector.nextLine();
+ 
+			while (lector.hasNextLine()) {
+				matriz[fila] = linea.split(","); //Separamos datos de la linea CSV leida e insertamos en matriz
+				fila++; //APuntamos a la siguiente fila
+				linea = lector.nextLine(); //Leemos siguiente línea
+			}
+			lector.close();
+			//Finalizada la lectura de archivo CSV, retornamos la matriz con los datos
+			return matriz;
+        }catch (FileNotFoundException e) {
+			System.out.println("No se encuentra fichero: " + rutaSCV);
+			return null;
+        }
+    }
+
+    public void mostrarAsignaciones(String[][] matriz) {
+
+        System.out.println("Ingrese ID de salón");
+        Scanner sc = new Scanner(System.in);
+        int idSalon = sc.nextInt();
+        String s = String.valueOf(idSalon); //Cambia e int a String
+
+		//Empezamos a recorrer matriz desde fila 1, la fila 0 es cabecera, no contiene datos
+		for (int fila = 1; fila < matriz.length; fila++) {
+			String nombre = matriz[fila][0]; //Recogemos nombre del estudiante de esta fila
+ 
+			if (nombre != null) //Si no es null, es que en esta fila de la matriz hay datos con los que trabajar
+				//Ahora recorremos columnas, si encontramos valor "0", mostramos nombre y número de asignacion
+				for (int col = 0; col < matriz[fila].length; col++)
+					if (matriz[fila][col].equals(s))
+						System.out.println(nombre + " --> " + matriz[0][col]); //Nombre asignacion lo obtenemos de la cabecera(fila 0)
+		}
+        sc.close();
+    }
 }
